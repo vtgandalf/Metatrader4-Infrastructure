814d69c8102348b43a0e1c0dc2c5599fc01f652d
Valeri Todorov <37157239+vtgandalf@users.noreply.github.com>
2019-08-24 23:45:39 +0200

    gui example update

--- examples/gui_example.py
+++ examples/gui_example.py
@@ -1,19 +1,81 @@
 import tkinter
+from tkinter import *
+import json
 
-window = tkinter.Tk()
-window.title("GUI")
+class Example(Frame):
+    json_file = 'gui.json'
+    algorithm_list = list()
+    window_elements_container = list()
+    parsed = None
+    with open(json_file) as f:
+        parsed = json.loads(f.read())
 
-# creating 2 text labels and input labels
+    def parse_algorithms(self, json_file):
+        for algorithm in self.parsed['algorithms']:
+            self.algorithm_list.append(algorithm['name'])
 
-tkinter.Label(window, text = "Username").grid(row = 0) # this is placed in 0 0
-# 'Entry' is used to display the input-field
-tkinter.Entry(window).grid(row = 0, column = 1) # this is placed in 0 1
+    def __init__(self):
+        super().__init__()
+        self.parse_algorithms(self.json_file)
+        self.initUI()
 
-tkinter.Label(window, text = "Password").grid(row = 1) # this is placed in 1 0
-tkinter.Entry(window).grid(row = 1, column = 1) # this is placed in 1 1
 
-# 'Checkbutton' is used to create the check buttons
-tkinter.Checkbutton(window, text = "Keep Me Logged In").grid(columnspan = 2) # 'columnspan' tells to take the width of 2 columns
-                                                                             # you can also use 'rowspan' in the similar manner
+    def initUI(self):
+        self.master.title("GUI")
+        # creating a root menu to insert all the sub menus
+        root_menu = tkinter.Menu(self.master)
+        self.master.config(menu = root_menu)
 
-window.mainloop()
+        # creating sub menus in the root menu
+        # it intializes a new sub menu in the root menu
+        actions_menu = tkinter.Menu(root_menu)
+        root_menu.add_cascade(label = "Actions", menu = actions_menu)
+        actions_menu.add_command(label = "Send", command = self.onSend)
+        actions_menu.add_command(label = "Exit", command = self.onExit)
+
+        algorithm_menu = tkinter.Menu(root_menu)
+        # it creates the name of the sub menu
+        root_menu.add_cascade(label = "Algorithms", menu = algorithm_menu)
+        i = 0
+        for algorithm in self.parsed['algorithms']:
+            algorithm_menu.add_command(label = algorithm['name'], command = lambda x=algorithm['name']: self.refreshUI(x))
+            i = i + 1
+        
+
+    def onSend(self):
+        pass
+
+    def onExit(self):
+        self.quit()
+
+
+    def refreshUI(self, name):
+        print(name)
+        for algorithm in self.parsed['algorithms']:
+            if algorithm['name'] == name:
+                for element in self.window_elements_container:
+                    if element:
+                        for widget in element:
+                            widget.destroy()
+                self.window_elements_container = list()
+                i = 0
+                for element in (algorithm['parameters']):
+                    name = list(element.keys())[0]
+                    label = tkinter.Label(self.master, text = name)
+                    label.grid(row = i)
+                    default = StringVar(self.master, value=element.get(name))
+                    text_box = tkinter.Entry(self.master, textvariable=default)
+                    text_box.grid(row = i, column = 1)
+                    self.window_elements_container.append([label,text_box])
+                    i = i + 1
+
+
+def main():
+    window = Tk()
+    # window.geometry("250x150+300+300")
+    app = Example()
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    main()
