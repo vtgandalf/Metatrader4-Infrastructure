9bbeb255420ef22dbc26c60e6f9c65e715302791
Valeri Todorov <v.v.todorow@gmail.com>
2019-08-25 14:20:25 +0200

    GUI and multithreading UClient

--- user_client/gui_example.py
+++ user_client/gui_example.py
@@ -1,14 +1,131 @@
 import os
 import sys
-sys.path.append(os.path.abspath('messages'))
+sys.path.append(os.path.abspath('messages_py'))
 
 import tkinter
 from tkinter import *
 import json
 import google.protobuf.json_format as json_format
 
+import grpc
+import time
+import threading
+from multiprocessing import Process
+from concurrent import futures
+
+import service_pb2 as service
+import service_pb2_grpc as service_grpc
+from google.protobuf.empty_pb2 import Empty
+
 from testing_data_pb2 import TestingData
 
+import base as base
+
+# GLOBALS
+testing_data_queue = []
+lock = threading.Lock()
+
+# THREADS
+gui_thread = threading.Thread()
+grpc_thread = threading.Thread()
+
+# SERVER
+grpc_timeout = 5
+
+class Listener(service_grpc.MetaTrader4ServiceServicer):
+    server_address = None
+    report_count = 0
+
+    def __inti__(self, *args, **kwargs):
+        self.lastPrintTime = time.time()
+
+    def set_result(self, report, context):
+        self.report_count = self.report_count + 1
+        print("Result from station({}):".format(str(report.station_id.value)))
+        print(report)
+        print("Report Count({})".format(str(self.report_count)))
+        return Empty()
+
+    def check_online(self, request, context):
+        return Empty()
+
+
+
+def client_action_set_testing_data(testing_data, address):
+    with grpc.insecure_channel(address) as channel:
+        stub = service_grpc.MetaTrader4ServiceStub(channel)
+        response = stub.set_testing_data(testing_data)
+        channel.unsubscribe(close)
+
+def client_check_online(address):
+    with grpc.insecure_channel(address) as channel:
+        stub = service_grpc.MetaTrader4ServiceStub(channel)
+        stub.check_online(Empty(), grpc_timeout)
+        channel.unsubscribe(close)
+        return
+
+def close(channel):
+    channel.close
+
+def serve():
+    listener = Listener()
+    listener.server_address = base.address_parser_server('./../addresses.json')
+
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    service_grpc.add_MetaTrader4ServiceServicer_to_server(listener, server)
+    server.add_insecure_port("[::]:9998")
+    server.start()
+
+    try:
+        while True:
+            queue_cpy = None
+            global testing_data_queue
+            lock.acquire()
+            if testing_data_queue:
+                queue_cpy = testing_data_queue
+            lock.release()
+            if queue_cpy:
+                for message in queue_cpy:
+                    try:
+                        client_check_online(listener.server_address)
+                    except Exception as err:
+                        # print(err)
+                        print("ERROR: Could not connect to server({})!".format(str(listener.server_address)))
+                    else:
+                        client_action_set_testing_data(message, listener.server_address)
+                        queue_cpy.remove(message)
+                        print("INFO: Testing data sent to server({}).".format(str(listener.server_address)))
+                        break
+                lock.acquire()
+                testing_data_queue = queue_cpy
+                lock.release()
+
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("INFO: KeyboardInterrupt.")
+        server.stop(0)
+
+def mock_testing_data():
+    x = TestingData()
+    x.symbol.value = 'EURUSD'
+    x.period.value = 'M5'
+    x.spread.value = '5'
+    x.time_period.initial_date.year.value = 2019
+    x.time_period.initial_date.month.value = 8
+    x.time_period.initial_date.day.value = 16
+    x.time_period.final_date.year.value = 2019
+    x.time_period.final_date.month.value = 8
+    x.time_period.final_date.day.value = 17
+    x.algorithm.name.value = 'Moving Average'
+    x.algorithm.parameters.Lots.value = float(0.1)
+    x.algorithm.parameters.MaximumRisk.value = float(0.02)
+    x.algorithm.parameters.DecreaseFactor.value = float(3.0)
+    x.algorithm.parameters.MovingPeriod.value = int(12)
+    x.algorithm.parameters.MovingShift.value = int(6)
+    return x
+# END SERVER
+
+# FORM
 class Example(Frame):
     json_file = 'gui.json'
     window_elements_container = list()
@@ -44,7 +161,6 @@ class Example(Frame):
         actions_menu = tkinter.Menu(root_menu)
         root_menu.add_cascade(label = "Actions", menu = actions_menu)
         actions_menu.add_command(label = "Send", command = self.onSend)
-        actions_menu.add_command(label = "Exit", command = self.onExit)
 
         algorithm_menu = tkinter.Menu(root_menu)
         # it creates the name of the sub menu
@@ -56,7 +172,6 @@ class Example(Frame):
         
         i = 0
         for element in self.parsed['common']:
-            print(type(element))
             try:
                 self.parsed['common'][element][0]
             except:
@@ -86,7 +201,7 @@ class Example(Frame):
 
         b = Button(self.master, text="Send", command = self.onSend).grid(row = i)
 
-    def updateJson(self):
+    def getMessageFromJson(self):
         for element in self.parsed['common']:
             try:
                 self.parsed['common'][element][0]
@@ -103,7 +218,6 @@ class Example(Frame):
                         break
             
         if self.chosen_algorithm:
-            print(self.chosen_algorithm)
             i = 0
             for algorithm in self.parsed['algorithms']:
                 if algorithm['name'] == self.chosen_algorithm:
@@ -146,24 +260,24 @@ class Example(Frame):
                 else: 
                     if el == "name":
                         algorithm_dict.update({el: {"value": algorithm[el]}})
-                
-                print(algorithm_dict)
+
                 dict_data.update({"algorithm": algorithm_dict})
         
         js = json.dumps(dict_data)
         message = TestingData()
         json_format.Parse(js, message, ignore_unknown_fields=False)
         print(message)
+        return(message)
 
 
     def onSend(self):
         if self.chosen_algorithm:
-            self.updateJson()
-        # pass
-
-    def onExit(self):
-        self.quit()
-
+            message = self.getMessageFromJson()
+            global testing_data_queue
+            lock.acquire()
+            testing_data_queue.append(message)
+            lock.release()
+            
 
     def refreshUI(self, name):
         self.chosen_algorithm = name
@@ -187,7 +301,14 @@ class Example(Frame):
                     self.window_elements_container.append([label,text_box])
                     self.parameter_list.append([name, text_box])
                     i = i + 1
-
+# END FORM
+def init_threads():
+    print("start gui")
+    gui_thread = threading.Thread(target=main).start()
+    print("start grpc")
+    grpc_thread = threading.Thread(target=serve).start()
+    # gui_thread
+    # grpc_thread
 
 def main():
     window = Tk()
@@ -195,6 +316,5 @@ def main():
     app = Example()
     window.mainloop()
 
-
 if __name__ == '__main__':
-    main()
+    init_threads()
